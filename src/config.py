1a6b2aa5e20db3c89d01c5ca342aa37cea521a86
Kristian Kjerstad <kristian.kjerstad@brilliant.no>
2024-06-07 10:29:47 +0200

    add pre commit hooks and rename folder to src

---
+++ src/config.py
@@ -0,0 +1,14 @@
+import os
+
+
+def get_env_var(key: str, allow_none=False) -> str | None:
+    env_value = os.getenv(key, None)
+    if env_value is None and allow_none is False:
+        raise Exception("No env variable found")
+    return env_value
+
+class Config:
+    DJANGO_SECRET_KEY = get_env_var("DJANGO_SECRET_KEY")
+
+
+config = Config()
