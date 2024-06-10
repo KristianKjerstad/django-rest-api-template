d3037f88c36921ab95430c766cc04fc54edce43d
Kristian Kjerstad <kristian.kjerstad@brilliant.no>
2024-06-10 15:47:38 +0200

    add auth

--- src/apitemplate/urls.py
+++ src/apitemplate/urls.py
@@ -1,14 +1,29 @@
 from django.contrib import admin
 from django.urls import path
 from ninja import NinjaAPI
-
+from ninja.security import django_auth, HttpBearer
 
 from tracks.api import router as tracks_router
 from apitemplate.api import router as api_router
 
-api = NinjaAPI()
+class InvalidToken(Exception):
+    pass
+
+
+class GlobalAuth(HttpBearer):
+    def authenticate(self, request, token):
+        if token == "supersecret":
+            return token
+        raise InvalidToken
+
+api = NinjaAPI(csrf=True, auth=GlobalAuth())
 api.add_router("/tracks", tracks_router)
 api.add_router("/api", api_router)
+
+@api.exception_handler(InvalidToken)
+def on_invalid_token(request, exc):
+    return api.create_response(request, {"detail": "Invalid token supplied"}, status=401)
+
 
 urlpatterns = [
     # path("admin/", admin.site.urls),
